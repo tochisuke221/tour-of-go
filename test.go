e82c50a0e475c17b19b49b34eddcb6b5f8181dab
tochisuke221 <tochisuke221@gmail.com>
2024-06-29 11:48:05 +0900

    go if

--- test.go
+++ test.go
@@ -1,27 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func swap(x, y string) (string, string) {
-	return y, x
-}
-
-func split(sum int)(x, y int){
-	x = sum * 4 / 9
-	y = sum - x
-	return
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
-
-	fmt.Println(split(17))
-
-
-	var i, j int = 1, 3
-	k := 3
-	c, python, java := true, false, "no!"
-
-	fmt.Println(i, j, k, c, python, java)
+	fmt.Println(sqrt(2), sqrt(-4))
 }
