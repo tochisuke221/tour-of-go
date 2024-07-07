1845420f8e84aae332a096fa0f528961fbc3b09c
tochisuke221 <tochisuke221@gmail.com>
2024-07-07 16:28:55 +0900

    ポインタ

--- sudoku.go
+++ sudoku.go
@@ -2,154 +2,39 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
-	"slices"
 )
-
+	
 func main() {
-	const message = "Hello, World!"
+	for i :=0 ; i < 10; i++ {
+		hantei(&i)
+  }
 
-	println(message)
-
-	// const (
-	// 	a = 1 + 2
-	// 	b
-	// 	c
-	// 	// The number of rows and columns in the grid
-	// )
-
-	//  println(a, b, c)
+	n, m := 10, 20
+	swap2(&n, &m)
+	// println(n, m)
+	
+	println(n, m)
 
 
-	 const (
-		Draft = iota
-		Open
-		Pending
-	 )
-	 
-	 println(Draft, Open, Pending)
 
-	 x := 3
-	 if x == 1 {
-		println("x is 1")
-	 }else if x == 2 {
-		println("x is 2")
-	}else{
-			println("x is not 1 or 2")
+}
+
+func hantei(i *int) {
+	if *i%2 == 0 {
+		fmt.Printf("%d-偶数\n", *i)
+	} else {
+		fmt.Printf("%d-奇数\n", *i)
 	}
+}
 
-	if z := 5; z < 10 {
-		println("z is less than 10")
-	}
+func swap(a, b int)(int, int){
+	return b, a
+}
 
-	var test []int= []int{1, 2}
+func swap2(a, b *int)(int, int){
+	var tmp = *a
+	*a = *b
+	*b = tmp
 
-	println(test)
-
-	for i, v := range []int{1,2,3} {
-		println(i, v)
-	}
-
-	for i := 0; i< 10; i++ {
-		println(i)
-	}
-
-	// for i := 1; i < 101; i++ {
-	// 	if i%2 == 0 {
-	// 		fmt.Printf("%d-偶数\n", i)
-	// 	}else{
-	// 		fmt.Printf("%d-奇数\n", i)
-	// 	}
-	// }
-
-
-
-	t := time.Now().UnixNano()
-	rand.Seed(t)
-	if result := rand.Intn(6); result%6 == 0 {
-		fmt.Println("大吉")
-	} else if result%5 == 0 || result%4 == 0{
-		fmt.Println("中吉")
-	} else if result%3 == 0 || result%2 == 0{
-		fmt.Println("吉")
-	} else{
-		fmt.Println("凶")
-	}
-
-	var f float64 = 10
-	var n int = int(f)
-	println(n)
-
-
-	sum := 5 + 6 + 3
-
-	avg := float64(sum / 3)
-
-	if avg > 2.5 {
-		println("good")
-	}else {
-		println("bad")
-	}
-
-	// var p struct {
-	// 	name string
-	// 	age int
-	// }
-
-
-	// p := struct {
-	// 	name string
-	// 	age int
-	// }{
-	// 	name: "gopher",
-	// 	age: 10,
-	// }
-
-	// println(p.name, p.age)
-
-	// ns3 := [5]int{10, 20, 30, 40, 50}
-
-	// println(ns3[1:3])
-
-	// ns := make([]int, 5, 10)
-	// println(ns)
-	// println(len(ns))
-	// println(cap(ns))
-
-	// ns = append(ns, 10, 10)
-	// println(ns)
-	// println(len(ns))
-	// println(cap(ns))
-
-	ns := []int{10, 20, 30, 40, 50}
-	n, m := 2, 4
-
-	fmt.Println(ns[n:])
-	fmt.Println(ns[:m])
-
-	ms := ns[:m:5]
-
-	fmt.Println(cap(ms))
-
-	ns = slices.Delete(ns, 1, 3)
-
-	fmt.Println(ns)
-
-	ns = slices.Insert(ns, 1, 100, 200)
-
-	fmt.Println(ns)
-
-	fmt.Println(slices.Contains(ns, 100))
-
-	slices.Sort(ns)
-	fmt.Println(ns)
-
-
-
-
-
-
-
-		
+	return *a, *b
 }
