69cb415456332a94a45a0dcfbe3d0804824c694f
tochisuke221 <tochisuke221@gmail.com>
2024-07-07 14:02:52 +0900

    色々

--- sudoku.go
+++ sudoku.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
+	"slices"
 )
 
 func main() {
@@ -63,7 +65,8 @@ func main() {
 
 
 
-
+	t := time.Now().UnixNano()
+	rand.Seed(t)
 	if result := rand.Intn(6); result%6 == 0 {
 		fmt.Println("大吉")
 	} else if result%5 == 0 || result%4 == 0{
@@ -73,6 +76,80 @@ func main() {
 	} else{
 		fmt.Println("凶")
 	}
+
+	var f float64 = 10
+	var n int = int(f)
+	println(n)
+
+
+	sum := 5 + 6 + 3
+
+	avg := float64(sum / 3)
+
+	if avg > 2.5 {
+		println("good")
+	}else {
+		println("bad")
+	}
+
+	// var p struct {
+	// 	name string
+	// 	age int
+	// }
+
+
+	// p := struct {
+	// 	name string
+	// 	age int
+	// }{
+	// 	name: "gopher",
+	// 	age: 10,
+	// }
+
+	// println(p.name, p.age)
+
+	// ns3 := [5]int{10, 20, 30, 40, 50}
+
+	// println(ns3[1:3])
+
+	// ns := make([]int, 5, 10)
+	// println(ns)
+	// println(len(ns))
+	// println(cap(ns))
+
+	// ns = append(ns, 10, 10)
+	// println(ns)
+	// println(len(ns))
+	// println(cap(ns))
+
+	ns := []int{10, 20, 30, 40, 50}
+	n, m := 2, 4
+
+	fmt.Println(ns[n:])
+	fmt.Println(ns[:m])
+
+	ms := ns[:m:5]
+
+	fmt.Println(cap(ms))
+
+	ns = slices.Delete(ns, 1, 3)
+
+	fmt.Println(ns)
+
+	ns = slices.Insert(ns, 1, 100, 200)
+
+	fmt.Println(ns)
+
+	fmt.Println(slices.Contains(ns, 100))
+
+	slices.Sort(ns)
+	fmt.Println(ns)
+
+
+
+
+
+
 
 		
 }
