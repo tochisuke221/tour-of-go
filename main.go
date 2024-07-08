b4c66fc625676fb0090306f41cd1e5080af0220d
tochisuke221 <tochisuke221@gmail.com>
2024-07-08 21:50:42 +0900

    catコマンド

--- main.go
+++ main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
-var msg = flag.String("msg", "デフォルト値", "メッセージ")
-var n int
-
-func init() {
-	// ポインタを指定して設定を予約
-	flag.IntVar(&n, "n", 1, "回数")
-}
-
-
 func main() {
-  fmt.Println(os.Args)
+	showLineNumbers := flag.Bool("n", false, "show line numbers")
+	flag.Parse()
 
-  flag.Parse()
+	filePaths := flag.Args()
 
-  fmt.Println(strings.Repeat(*msg, n))
+	if len(filePaths) == 0 {
+		fmt.Println("Usage: mycat [-n] file1 file2 ...")
+		
+		return
+	}
+
+	lineNumber := 1
+
+	for _, filePath := range filePaths {
+		file, err := os.Open(filePath)
+		
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening file %s: %v\n", filePath, err)
+			continue
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if *showLineNumbers {
+				fmt.Printf("%d: %s\n", lineNumber, line)
+				lineNumber++
+			} else {
+				fmt.Println(line)
+			}
+		}
+
+		// エラーチェック
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file %s: %v\n", filePath, err)
+		}
+		
+
+	}
+
+
+
 }
-
