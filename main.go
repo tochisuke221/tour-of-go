5df7d33c3a1c2ab47005fd957e9db00ceef9a3dd
tochisuke221 <tochisuke221@gmail.com>
2024-07-11 22:38:50 +0900

    ゴールーチン

--- main.go
+++ main.go
@@ -1,51 +1,19 @@
 package main
 
 import (
-	"bytes"
-	"errors"
 	"fmt"
-	"io"
-	"log"
-	"strings"
-	"unicode/utf8"
+	"time"
 )
 
-type RuneScanner struct {
-	r   io.Reader
-	buf [16]byte
-}
-
-func NewRuneScanner(r io.Reader) *RuneScanner {
-	return &RuneScanner{r: r}
-}
-
-func (s *RuneScanner) Scan() (rune, error) {
-	n, err := s.r.Read(s.buf[:])
-	if err != nil {
-		return 0, err
-	}
-
-	r, size := utf8.DecodeRune(s.buf[:n])
-	if r == utf8.RuneError {
-		return 0, errors.New("RuneError")
-	}
-
-	s.r = io.MultiReader(bytes.NewReader(s.buf[size:n]), s.r)
-	return r, nil
-}
-
 func main() {
-	s := NewRuneScanner(strings.NewReader("Hello, 世界世界世界世界世界世界世界世界世界世界世界世界世界世界世界世界世界"))
-	for {
-		r, err := s.Scan()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%c\n", r)
-	}
+	defer fmt.Println("main done")
+	go func() {
+		defer fmt.Println("goroutine1 done")
+		time.Sleep(3 * time.Second)
+	}()
+	go func() {
+		defer fmt.Println("goroutine2 done")
+		time.Sleep(1 * time.Second)
+	}()
+	time.Sleep(5 * time.Second)
 }
