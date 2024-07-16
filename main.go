bbf062d6642bac21b068e3b14799656179366916
tochisuke221 <tochisuke221@gmail.com>
2024-07-16 22:10:28 +0900

    色々

--- main.go
+++ main.go
@@ -1,19 +1,84 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	ch1 := make(chan int)
-	var ch2 chan string
-	go func() { ch1 <- 100 }()
-	go func() { ch2 <- "hi" }()
+type Employee struct{
+    ID int
+    Name string
+    Age int
+    Sex string
+}
 
-	select {
-	case v1 := <-ch1:
-		fmt.Println(v1)
-	case v2 := <-ch2:
-		fmt.Println(v2)
-	}
+type PEmployee struct{
+    ID *int
+    Name *string
+    Age *int
+    Sex *string
+}
+
+func convertPEmployee(employees []Employee) []PEmployee{
+
+    var pEmployees []PEmployee
+    for _,employee := range employees{
+        pEmployees = append(pEmployees,PEmployee{
+            ID: &employee.ID,
+            Name: &employee.Name,
+            Age: &employee.Age,
+            Sex: &employee.Sex,
+        })
+    }
+    return pEmployees
+
+	    var pEmployees []PEmployee
+        for _,employee := range employees{
+
+        fmt.Println("==",i+1," loop==")
+        fmt.Println("&i = ",&i)
+        fmt.Println("&employee.ID = ",&employee.ID)
+        fmt.Println("&employee.Name = ",&employee.Name)
+        fmt.Println("&employee.Age = ",&employee.Age)
+        fmt.Println("&employee.Sex = ",&employee.Sex)
+        fmt.Println("&employee = ",employee)
+        pEmployees = append(pEmployees,PEmployee{
+            ID: &employee.ID,
+            Name: &employee.Name,
+            Age: &employee.Age,
+            Sex: &employee.Sex,
+        })
+    }
+    return pEmployees
+}
+
+func main(){
+
+    employees := []Employee{
+        Employee{
+            ID:   1,
+            Name: "山田太郎",
+            Age:  23,
+            Sex:  "男",
+        },
+        Employee{
+            ID:   2,
+            Name: "山田花子",
+            Age:  27,
+            Sex:  "女",
+        },
+        Employee{
+            ID:   3,
+            Name: "田中次郎",
+            Age:  36,
+            Sex:  "男",
+        },
+    }
+    pEmployees := convertPEmployee(employees)
+
+    fmt.Println("==PEmployees==")
+    for _,pEmployee := range pEmployees{
+        fmt.Println("ID = ",*pEmployee.ID)
+        fmt.Println("Name = ",*pEmployee.Name)
+        fmt.Println("Age = ",*pEmployee.Age)
+        fmt.Println("Sex = ",*pEmployee.Sex)
+        fmt.Println("--")
+    }
 }
